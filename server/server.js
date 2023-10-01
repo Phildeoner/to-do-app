b3337196f188cc56b46866db7f224fb098521001
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-10-01 22:46:13 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5000;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/todoApp", {
+mongoose.connect("mongodb://127.0.0.1:27017/todoApp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
