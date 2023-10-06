e833fb9d4862eaa089871cc31345986466b9851c
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-10-06 22:35:28 +0100

    Configure flowbite and add skeleton when loadinjg response

--- client/src/components/AiGenerate.jsx
+++ client/src/components/AiGenerate.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Skeleton from "./Skeleton";
 
 function AiGenerate({ onTodoAdded }) {
   const [userInput, setUserInput] = useState("");
   const [aiTodos, setAiTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const saveTodoToServer = async (todo) => {
     try {
@@ -14,10 +16,13 @@ function AiGenerate({ onTodoAdded }) {
       onTodoAdded(response.data); // Update the main Todo list
     } catch (error) {
       console.error("Error saving to-do to the server:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const createTodoList = async () => {
+    setLoading(true);
     try {
       const response = await axios.post("http://localhost:5000/create-todo", {
         userInput,
@@ -36,17 +41,23 @@ function AiGenerate({ onTodoAdded }) {
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-3 mb-10">
-        <textarea
-          rows="4"
-          class="block p-2.5 w-[80vw] md:w-[40vw] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  "
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Describe the to-do list you want..."></textarea>
-        <button
-          className="h-12 border px-10 w-full shadow-md rounded-md bg-red-500 hover:bg-red-600 text-white font-bold"
-          onClick={createTodoList}>
-          Generate To-Do List
-        </button>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <>
+            <textarea
+              rows="4"
+              class="block p-2.5 w-[80vw] md:w-[40vw] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-700 focus:border-gray-700"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              placeholder="Describe the to-do list you want..."></textarea>
+            <button
+              className="h-12 border px-10 w-full shadow-md rounded-md bg-red-500 hover:bg-red-600 text-white font-bold"
+              onClick={createTodoList}>
+              Generate To-Do List
+            </button>
+          </>
+        )}
       </div>
     </>
   );
